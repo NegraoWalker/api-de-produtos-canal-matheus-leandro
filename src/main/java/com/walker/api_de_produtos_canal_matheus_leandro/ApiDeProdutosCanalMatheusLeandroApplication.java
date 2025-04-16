85d0df1da0fb768c4247639c921e57006d669e68
Walker Negrao <1882452@tcs.com>
2025-04-16 10:07:02 -0300

    Criação do repositório

---
+++ src/main/java/com/walker/api_de_produtos_canal_matheus_leandro/ApiDeProdutosCanalMatheusLeandroApplication.java
@@ -0,0 +1,13 @@
+package com.walker.api_de_produtos_canal_matheus_leandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeProdutosCanalMatheusLeandroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeProdutosCanalMatheusLeandroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walker/api_de_produtos_canal_matheus_leandro/ApiDeProdutosCanalMatheusLeandroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.api_de_produtos_canal_matheus_leandro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeProdutosCanalMatheusLeandroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
